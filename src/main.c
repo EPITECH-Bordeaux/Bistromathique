832313b371edb51e78395a0e10371cceb97a7f8b
Nathan Poirier <nathan@poirier.io>
2015-05-27 00:09:49 +0200

    remove useless new line

--- src/main.c
+++ src/main.c
@@ -6,9 +6,7 @@ int		main(int ac, char **av)
 
   if ((data = init()) == NULL)
     return (EXIT_FAILURE);
-  
   data->pars.btree->aff = MY_TRUE;
-
   reader_readfd(data, 0, NULL);
   return (EXIT_SUCCESS);
 }
