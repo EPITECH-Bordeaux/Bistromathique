500613c5f8668e9252be23c8158301f339803670
greil_t <timothy.greil@epitech.eu>
2015-05-30 17:11:09 +0200

    fix error calc

--- src/calc/calc_mul.c
+++ src/calc/calc_mul.c
@@ -2,7 +2,7 @@
 
 bool	calc_mul(t_bistro *bi, t_nb_op **pile_nb)
 {
-  (*pile_nb)->next->nbr = (*pile_nb)->nbr * (*pile_nb)->next->nbr;
+  (*pile_nb)->next->nbr = (*pile_nb)->next->nbr * (*pile_nb)->nbr;
   (*pile_nb) = (*pile_nb)->next;
   return (false);
 }
